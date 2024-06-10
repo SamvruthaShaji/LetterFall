5c2260c4e2957348f34516c8dcc9a63c139c7e30
Abhiram S Anand <abhiram.anand@experionglobal.local>
2024-06-10 23:18:06 +0530

    All linked

--- AccountCreated/accountcreated.js
+++ AccountCreated/accountcreated.js
@@ -1,4 +1,4 @@
 document.getElementById('button1').addEventListener('click',gotowebsite)
 function gotowebsite(){
-    window.location.href = '../Integrated/Game.html'
+    window.location.href = '../Ready/Ready.html'
 }
--- signin/signin.js
+++ signin/signin.js
@@ -1,36 +1,7 @@
-{/* <script>
-        function validate() {
-            var username = document.getElementById("Name").value;
-            var password = document.getElementById("Password").value;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const signinButton = document.getElementById('signin-btn');
 
-            // Check if username is empty
-            if (username.trim() === "") {
-                alert("Username cannot be empty");
-                return false;
-            }
-
-            // Check if password is empty
-            if (password.trim() === "") {
-                alert("Password cannot be empty");
-                return false;
-            }
-
-            // Check if password length is less than 6 characters
-            if (password.length < 6) {
-                alert("Password must be at least 6 characters long");
-                return false;
-            }
-
-            // Validation passed
-            alert("Validation passed!");
-            return true;
-        }
-    </script> */}
-
-    document.addEventListener('DOMContentLoaded', (event) => {
-        const signupButton = document.getElementById('signin-btn');
-    
-        signupButton.addEventListener('click', () => {
-            window.location.href = '../Ready/Ready.html';
-        });
+    signinButton.addEventListener('click', () => {
+        window.location.href = '../Ready/Ready.html';
     });
+});
