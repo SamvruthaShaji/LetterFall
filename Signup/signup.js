7675960b4556a5063d7afeffdb1db197ff0fbcf6
Sahir Nisar <sahir.nisar@experionglobal.local>
2024-06-10 13:37:31 +0530

    signup id name changed

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const signupButton = document.getElementById('signup-btn');
+    const signupButton = document.getElementById('signUp');
 
     signupButton.addEventListener('click', () => {
         window.location.href = 'newpage.html';
