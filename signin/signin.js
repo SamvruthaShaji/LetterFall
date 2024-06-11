907da5689dbdbee69696d0c8f7dc6032869c47f6
Vibijith <vibijithkongat@gmail.com>
2024-06-11 10:11:38 +0530

    changes in button id and added script tag

--- signin/signin.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const signinButton = document.getElementById('signin-btn');
-
-    signinButton.addEventListener('click', () => {
-        window.location.href = '../Ready/Ready.html';
-    });
-});
