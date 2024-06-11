517d01f4fea17f894b88ed5c3d60d14ad7cd3ea3
Abhiram S Anand <abhiram.anand@experionglobal.local>
2024-06-12 00:40:11 +0530

    Everything done

--- Signup/signup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const signupButton = document.getElementById('signup-btn');
-
-    signupButton.addEventListener('click', () => {
-        window.location.href = '../AccountCreated/accountcreated.html';
-    });
-});
